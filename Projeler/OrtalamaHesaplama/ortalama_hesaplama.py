d0cdb6d05afcb7337355d4b3a7ae31f04d54ad46
kayra <51532778+formalite@users.noreply.github.com>
2023-05-23 22:45:35 +0300

    Create ortalama_hesaplama.py

---
+++ Projeler/OrtalamaHesaplama/ortalama_hesaplama.py
@@ -0,0 +1,7 @@
+not1 = int(input("Lütfen 1. sınav notunuzu girin: "))
+not2 = int(input("Lütfen 1. sınav notunuzu girin: "))
+ortalama = (not1 + not2)/2
+if ortalama >= 50:
+    print(Geçtiniz. Ortalamanız:", ortalama)
+else:
+    print(Kaldınız. Ortalamanız:", ortalama)
